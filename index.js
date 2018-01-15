ba181ad183db2f527bbdbc36d62495090d8a88cc
Jordan Jaffe <jordan.m.jaffe@gmail.com>
2018-01-15 23:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ function inc(n){
 function divide(a, b){
   return a / b
 }
+function  makeInt(n){
+  parseInt(n)
+}
+
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
